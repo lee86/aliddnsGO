c3abe65fd0d4ec6c0eb5ce63cc0fdaa0aaac09b7
Jiangwe Leo <15872426870@163.com>
2023-10-31 16:06:59 +0800

    将main拆分

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/zeromicro/go-zero/core/conf"
 	"os"
 )
@@ -38,60 +37,6 @@ func main() {
 			CertCheck()
 		default:
 			return
-		}
-	}
-}
-
-func Ipv6DDNS() {
-	localIpv6, err := GetOutBoundIPV6()
-	if err != nil {
-		return
-	}
-	qs.Ipv6 = localIpv6
-
-	qs.CreateClient()
-	checkError(err)
-	qs.queryDomain()
-	if len(qs.Body.DomainRecords.Record) == 0 {
-		log.Infof("本地ip:%v\t远端不存在此解析\t现在开始添加!...", qs.Ipv6)
-		qs.addDomain("AAAA", qs.Ipv6)
-		return
-	}
-	for _, record := range qs.Body.DomainRecords.Record {
-		if record.Type == "AAAA" {
-			if qs.Ipv6 == record.Value {
-				log.Infof("本地ip:%v\t远端ip:%v\t无需更新", qs.Ipv6, record.Value)
-				return
-			} else {
-				log.Infof("本地ip:%v\t远端ip:%v\t现在更新", qs.Ipv6, record.Value)
-				qs.updateDomain("AAAA", qs.Ipv6)
-				return
-			}
-		}
-	}
-}
-
-func CertCheck() {
-	qs.SubDomain = dnsHeader + qs.SubDomain
-	qs.Ipv6 = *Key
-	fmt.Println(qs.Ipv6)
-	qs.CreateClient()
-	qs.queryDomain()
-	if len(qs.Body.DomainRecords.Record) == 0 {
-		log.Infof("本地ip:%v\t远端不存在此解析\t现在开始添加!...", qs.Ipv6)
-		qs.addDomain("TXT", qs.Ipv6)
-		return
-	}
-	for _, record := range qs.Body.DomainRecords.Record {
-		if record.Type == "TXT" {
-			if qs.Ipv6 == record.Value {
-				log.Infof("本地ip:%v\t远端ip:%v\t无需更新", qs.Ipv6, record.Value)
-				return
-			} else {
-				log.Infof("本地ip:%v\t远端ip:%v\t现在更新", qs.Ipv6, record.Value)
-				qs.updateDomain("TXT", qs.Ipv6)
-				return
-			}
 		}
 	}
 }

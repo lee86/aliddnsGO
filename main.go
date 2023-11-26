ee3a818986c50142914c76c6bfd01611ccdab558
jiangwe leo <1099986768@qq.com>
2023-11-26 19:11:58 +0800

    优化代码，增加可读性

--- main.go
+++ main.go
@@ -3,17 +3,18 @@
 package main
 
 import (
+	"aliDDNS/aliddns"
 	"flag"
 	"github.com/zeromicro/go-zero/core/conf"
 	"os"
 )
 import "github.com/sirupsen/logrus"
 
-var qs = new(QueryStruct)
-var isTest = flag.Bool("t", false, "测试")
-var configFile = flag.String("f", "/etc/ddns/config.yaml", "the config file")
-var renewType = flag.String("rt", "ipv6", "设置certbot更新txt或者设置更新ipv6")
-var Key = flag.String("k", os.Getenv("CERTBOT_VALIDATION"), "")
+var qs = new(aliddns.QueryStruct)
+var isTest = flag.Bool("t", false, "测试用例，开启测试")
+var configFile = flag.String("f", "/etc/ddns/config.yaml", "阿里云配置文件")
+var renewType = flag.String("rt", "ipv6", "设置检查类型，支持ipv4,cert,ipv6三种")
+var Key = flag.String("k", os.Getenv("CERTBOT_VALIDATION"), "自动获取系统certbot参数，也可以进行传参")
 var log = logrus.New()
 
 // var remoteIpv4 = ""
@@ -23,6 +24,7 @@ var account Account
 func main() {
 	flag.Parse()
 	if flag.Parsed() {
+		qs.IsTest = *isTest
 		//fmt.Println(*Key, *renewType)
 		conf.MustLoad(*configFile, &account)
 
@@ -31,12 +33,19 @@ func main() {
 		qs.MainDomain = account.MainDomain
 		qs.SubDomain = account.SubDomain
 		switch *renewType {
+		case "ipv4":
+			qs.ValueType = "A"
+			qs.GetOutBoundIPV4()
 		case "ipv6":
-			Ipv6DDNS()
+			qs.ValueType = "AAAA"
+			qs.GetOutBoundIPV6()
 		case "cert":
-			CertCheck()
+			qs.ValueType = "TXT"
+			qs.SubDomain = dnsHeader + qs.SubDomain
+			qs.Value = *Key
 		default:
 			return
 		}
+		qs.DnsCheck()
 	}
 }

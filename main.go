510ce66c6c7e84d1a541342a8062e58729491f93
Jiangwe Leo <15872426870@163.com>
2023-12-08 11:47:44 +0800

    减少全局参数

--- main.go
+++ main.go
@@ -9,25 +9,26 @@ import (
 	"os"
 )
 
-var qs = new(aliddns.QueryStruct)
 var isTest = flag.Bool("t", false, "测试用例，开启测试")
 var renewType = flag.String("rt", "ipv6", "设置检查类型，支持ipv4,cert,ipv6三种")
 var Key = flag.String("k", os.Getenv("CERTBOT_VALIDATION"), "自动获取系统certbot参数，也可以进行传参")
 
-var dnsHeader = "_acme-challenge."
-var account Account
-
 func main() {
 	flag.Parse()
 	if flag.Parsed() {
-		qs.IsTest = *isTest
+		var qs = new(aliddns.QueryStruct)
+		var account Account
+		var dnsHeader = "_acme-challenge."
 
 		conf.MustLoad(*configFile, &account)
 
+		// 传参
+		qs.IsTest = *isTest
 		qs.AccessKeyId = account.AccessKeyId
 		qs.AccessSecret = account.AccessSecret
 		qs.MainDomain = account.MainDomain
 		qs.SubDomain = account.SubDomain
+
 		switch *renewType {
 		case "ipv4":
 			qs.ValueType = "A"
